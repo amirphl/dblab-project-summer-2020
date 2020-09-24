b8aba21cb4a6c31047162491177d644b839385ac
amirmohammad pirhosseinloo <amir.hosseinloo@cafebazaar.ir>
2020-09-24 18:08:41 +0330

    Add fake data

---
+++ users-24-09-2020-12-53-beta.sql
@@ -0,0 +1,134 @@
+﻿-- Generation time: Thu, 24 Sep 2020 12:53:00 +0000
+-- DB name: u574849695_22
+/*!40030 SET NAMES UTF8 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO users VALUES ('1','Margot Labadie MD','1-766-389-7413x133','abins@example.net'),
+('2','Brooklyn Rosenbaum','1-970-948-9510','abdul59@example.com'),
+('3','Burdette Waelchi','048-523-5653x3182','watsica.jovani@example.net'),
+('4','Mrs. Christina Mueller','+84(6)3682839579','kelli.harber@example.org'),
+('5','Dr. Conor Terry','392-266-3155','skirlin@example.org'),
+('6','Lily Marquardt Jr.','1-328-465-3044x35964','hbotsford@example.org'),
+('7','Dr. Nyasia Abernathy II','1-504-478-7942x88579','vcremin@example.com'),
+('8','Ms. Susie Cronin','+15(9)4525461934','tina32@example.com'),
+('9','Hildegard Bogan','277-992-6370x159','haley.chanel@example.org'),
+('10','Miss Kelsie Heller IV','(769)942-2375x5721','yundt.kylee@example.net'),
+('11','Maryse Homenick','(128)070-0510','brakus.juliet@example.org'),
+('12','Gayle Johnston','00221109361','hreynolds@example.org'),
+('13','Reyna Harris','(801)060-8405x985','chaya12@example.net'),
+('14','Clyde Ryan','1-589-516-0749','teresa62@example.com'),
+('15','Dr. Damon Douglas','1-176-822-3566','ebuckridge@example.org'),
+('16','Eloisa Hyatt DVM','(245)761-8661','simonis.breana@example.org'),
+('17','Aletha Breitenberg','(787)674-6722x2677','vanessa.padberg@example.net'),
+('18','Forest Roberts MD','1-713-357-5099x90464','schiller.vickie@example.com'),
+('19','Mr. Estevan Lehner','02514376621','stanton.michael@example.com'),
+('20','Emmett Wehner','1-237-316-8512x63350','ledner.allan@example.com'),
+('21','Eliane Ebert','711-756-4251x018','stoltenberg.judd@example.org'),
+('22','Syble Powlowski','(574)738-3511','althea58@example.org'),
+('23','Ryley Kunze','(865)879-9263x67677','kutch.isai@example.com'),
+('24','Marielle Pfannerstill','1-866-837-0341x3672','hane.bulah@example.com'),
+('25','Alberta Keebler','+15(9)8730741679','rosanna62@example.com'),
+('26','Royal Hagenes','(032)388-5512','schuyler.stoltenberg@example.org'),
+('27','Ms. Corene Sipes','652-466-0132','larkin.baylee@example.org'),
+('28','Mr. Ruben Rath','495.565.1882x669','lokeefe@example.org'),
+('29','Tad Walter','1-927-682-1707x21384','gleason.corene@example.org'),
+('30','Joan Nienow','(623)365-6618','presley73@example.com'),
+('31','Jessy McLaughlin','(131)295-9218','wade77@example.net'),
+('32','Ms. Ozella Schneider','893-895-3207x9461','sawayn.lee@example.net'),
+('33','Heidi Becker','1-636-808-9212x22976','glenna97@example.net'),
+('34','Prof. Ignatius Jacobi V','818-861-2127x777','ebony.bechtelar@example.org'),
+('35','Robyn Heidenreich','1-347-094-1145','onader@example.net'),
+('36','Prof. Kareem Lind DVM','1-426-266-6101x6348','randall.grady@example.net'),
+('37','Alicia Schuster','717-139-8352x308','pfannerstill.estrella@example.org'),
+('38','Ms. Sarah Bauch','(043)058-7467x739','bbeahan@example.org'),
+('39','Reilly Johns','1-702-062-6905x87810','wmiller@example.org'),
+('40','Elenora Walker','423-494-4071','yesenia86@example.com'),
+('41','Ceasar Emmerich','(723)705-8790x739','dmosciski@example.org'),
+('42','Jalon Hessel','1-752-519-6127','ayden03@example.net'),
+('43','Maeve Armstrong','309-231-1172x471','avandervort@example.net'),
+('44','Nicola Rowe','267-245-0027','mafalda90@example.com'),
+('45','Ahmed Rau','426-345-1787x530','kovacek.susana@example.net'),
+('46','Miss Blanche Boehm','+53(3)8475533415','coy.sauer@example.com'),
+('47','Kennedi Christiansen I','(222)916-4359x946','delfina.hickle@example.net'),
+('48','Michale Dicki III','01330288819','stoltenberg.rosanna@example.org'),
+('49','Dr. Emanuel Windler','005-092-6784x03239','london72@example.org'),
+('50','Aryanna Stiedemann','657.464.6032','modesto.botsford@example.org'),
+('51','Dr. Edwardo Walsh DVM','(546)239-6640','aylin45@example.org'),
+('52','Alek Rodriguez','(005)654-0555','weissnat.louisa@example.com'),
+('53','Dena Rosenbaum','783.258.1063','margaret.runolfsdottir@example.org'),
+('54','Sydnee Fadel','(125)914-7365x09488','ruthie86@example.net'),
+('55','Mandy Yost','1-120-816-2867x48001','ghomenick@example.com'),
+('56','Miss Eileen Predovic','(968)674-1686','cole.floyd@example.com'),
+('57','Ms. Katelin Mills','1-127-230-0517','swift.willie@example.com'),
+('58','Otis Heathcote','998-240-5926x7622','loy79@example.net'),
+('59','Ramon Beatty','1-993-904-2106x8372','myrtle.hartmann@example.org'),
+('60','Cindy Kuhn','1-655-270-8366x98914','kaylee53@example.net'),
+('61','Nicklaus Konopelski III','1-609-357-7494x52408','kglover@example.com'),
+('62','Mrs. Vesta Swift','(595)218-8695x619','floyd.ankunding@example.com'),
+('63','Carley Reichert','150-582-1476x330','marc.swift@example.com'),
+('64','Ephraim Satterfield','091-353-5445x37647','carolina47@example.org'),
+('65','Connor Kohler','487.369.0814x8054','durgan.annabel@example.net'),
+('66','Prof. Weston OHara V','(038)915-5854','rcremin@example.com'),
+('67','Mr. Scotty Runolfsdottir II','+85(7)0962287547','catherine19@example.net'),
+('68','Helga Wiegand','03060011002','pmiller@example.com'),
+('69','Noble Hills IV','02521990174','wmraz@example.net'),
+('70','Gianni Wiegand','291.804.2761x0009','enos.langworth@example.net'),
+('71','Alfonso Lindgren','(480)416-7097x353','ibogan@example.com'),
+('72','Dr. Julius Runolfsson','1-049-464-1159x8792','xcorwin@example.net'),
+('73','Juanita Collins','(002)185-4266x5858','weldon35@example.net'),
+('74','Mitchel Kuhic','011.279.4180x085','willa46@example.com'),
+('75','Emma Rodriguez','(766)201-2754x94686','jennings00@example.net'),
+('76','Emerald Bahringer','572.524.3158','wiza.nannie@example.org'),
+('77','Ola Rutherford','1-270-338-7668','bridie.upton@example.com'),
+('78','Dr. Tyree Wiza','960.317.6006','raphaelle.dicki@example.com'),
+('79','Dulce Ferry','+62(5)8966733411','jschaefer@example.com'),
+('80','Orville Jerde','(789)062-0390','sandy97@example.net'),
+('81','Dr. Jerry Wiza Jr.','1-645-852-1307x30221','zschumm@example.org'),
+('82','Marielle Waters','(999)272-7710x9113','ibahringer@example.net'),
+('83','Sylvia Weber II','+46(7)3902247136','rstreich@example.net'),
+('84','Sierra Treutel','1-312-952-0395x1031','hagenes.lori@example.net'),
+('85','Margarita Murphy','356-839-5787x034','marie81@example.net'),
+('86','Ben Streich MD','1-004-240-9299','vincenza67@example.com'),
+('87','Zita Murazik','1-488-568-6451x08355','dare.kassandra@example.org'),
+('88','Jarrett Metz','215.802.0960x74120','ebert.jessica@example.com'),
+('89','Nia Leuschke','245.715.1720x1116','maverick.kerluke@example.net'),
+('90','Prof. Benjamin Donnelly','319.823.8481','marc20@example.org'),
+('91','Mr. Wilfrid Ferry','789-444-0657','simonis.fred@example.com'),
+('92','Sophia Runolfsson','1-238-512-4007x2022','eichmann.jennie@example.net'),
+('93','Florida Goodwin','01703161683','gpagac@example.net'),
+('94','Mr. Anastacio Bradtke Jr.','961.404.7779','armando.cronin@example.com'),
+('95','Osbaldo Hoppe II','1-971-328-9884x448','odell.lynch@example.org'),
+('96','Christa Crona PhD','+19(2)2807135739','ygreenfelder@example.net'),
+('97','Irwin Maggio','469-645-8936x74271','nikki17@example.net'),
+('98','Meda Bailey','832-218-9151x406','tokeefe@example.net'),
+('99','Miss Susie Moore Sr.','023-431-1569x17605','london88@example.net'),
+('100','Khalid Douglas','(850)341-6811x526','vicenta54@example.net'); 
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
